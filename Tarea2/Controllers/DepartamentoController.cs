79fc89803dbac9f1a39051f078f03c4b158c4c87
Norquis R Suárez <nolkis@gmail.com>
2019-12-05 14:14:57 -0400

    Tarea2-7
    Diplomado

---
+++ Tarea2/Controllers/DepartamentoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tarea2Models.Models;
+
+namespace Tarea2.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private EmpleadoDBEntities db = new EmpleadoDBEntities();
+
+        // GET: Departamento
+        public ActionResult Index()
+        {
+            return View(db.Departamentoes.ToList());
+        }
+
+        // GET: Departamento/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamento/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamento/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDDepart,Descripcion")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departamentoes.Add(departamento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(departamento);
+        }
+
+        // GET: Departamento/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamento/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDDepart,Descripcion")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(departamento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamento/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Departamento departamento = db.Departamentoes.Find(id);
+            db.Departamentoes.Remove(departamento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
